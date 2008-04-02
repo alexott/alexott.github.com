ad5b5cf867f21ee54bcf7e69ec51578b6593fa31
Alex Ott <alexott@gmail.com>
2008-04-02 16:20:06 +0200

    Commit changes

---
+++ generate-sitemap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# File: generate-sitemap.sh
+#
+# Created: Wednesday, April  2 2008 by Alex Ott
+#
+
+FILE=sitemap.xml
+
+echo '<?xml version="1.0" encoding="UTF-8"?>' > $FILE
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $FILE
+find . -name \*.html| xargs ls -ls |awk '{print "<url><loc>" $9 "</loc>\n<lastmod>" $7 "</lastmod></url>"}'|sed -e 's|\.\/|http://xtalk.msk.su/~ott/|' >> $FILE
+echo '</urlset>' >> $FILE
