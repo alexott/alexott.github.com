d3158067f152b712fe5c31eccbe9eb79388c05f5
Alex Ott <alexott@gmail.com>
2010-01-21 11:07:13 +0100

    move to new domain and mix bugfixes

--- generate-sitemap.sh
+++ generate-sitemap.sh
@@ -9,6 +9,6 @@ FILE=sitemap.xml
 
 echo '<?xml version="1.0" encoding="UTF-8"?>' > $FILE
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >> $FILE
-find . -name \*.html -a -type f -print0| xargs -0 ls -l --time-style='full-iso' |awk '{print "<url><loc>" $9 "</loc>\n<lastmod>" $6 "</lastmod></url>"}'|sed -e 's|\.\/|http://xtalk.msk.su/~ott/|' >> $FILE
-find . -name \*.pdf  -a -type f -print0| xargs -0 ls -l --time-style='full-iso' |awk '{print "<url><loc>" $9 "</loc>\n<lastmod>" $6 "</lastmod></url>"}'|sed -e 's|\.\/|http://xtalk.msk.su/~ott/|' >> $FILE
+find . -name \*.html -a -type f -print0| xargs -0 ls -l --time-style='full-iso' |awk '{print "<url><loc>" $9 "</loc>\n<lastmod>" $6 "</lastmod></url>"}'|sed -e 's|\.\/|http://alexott.net/|' >> $FILE
+find . -name \*.pdf  -a -type f -print0| xargs -0 ls -l --time-style='full-iso' |awk '{print "<url><loc>" $9 "</loc>\n<lastmod>" $6 "</lastmod></url>"}'|sed -e 's|\.\/|http://alexott.net/|' >> $FILE
 echo '</urlset>' >> $FILE
