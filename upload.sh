34545ebabea0176914315dd32549053e73d787a3
Alex Ott <alexott@gmail.com>
2012-06-13 09:26:49 +0200

    cleanup repo

--- upload.sh
+++ upload.sh
@@ -5,4 +5,21 @@
 # Created: Tuesday, September 28 2010 by Alex Ott
 #
 
-rsync -avrlpm --exclude-from=exclude-list --delete-excluded --delete-during --rsh=ssh ~/projects/my-page-muse/ alexott@simple-hosting.ru:/home/alexott/alexott.net/htdocs/
+OS=`uname -s`
+MPOINT=/Volumes/alexott.net/
+if [ "$OS" = "Darwin"  ]; then
+    MPOINT=/Volumes/alexott.net/   
+elif [ "$OS" = "Linux" ]; then
+    MPOINT=/mnt/alexott.net/
+else
+    echo "Unsupported OS! $OS"
+    exit 1
+fi
+if [ ! -d $MPOINT ]; then
+    mkdir $MPOINT
+fi
+if [ ! -f $MPOINT/index.html ]; then
+    s3fs www.alexott.net $MPOINT -olocal,ping_diskarb,volname=AlexOttNet
+fi
+
+rsync -avrlpm --exclude-from=exclude-list --delete-excluded --delete-during --rsh=ssh ~/projects/my-page-muse/ $MPOINT
